724c91e39df8fb6d5878a7716dbac528db7434bb
Junior Trojilio <junior@Juniors-MacBook-Pro.local>
2020-05-11 08:32:46 -0300

    Challenge Solved

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import api from './services/api'
 
 import {
   SafeAreaView,
@@ -8,48 +9,60 @@ import {
   StatusBar,
   StyleSheet,
   TouchableOpacity,
+  Alert,
 } from "react-native";
 
 export default function App() {
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+      api.get('repositories').then(response =>{
+        setRepositories(response.data)      
+      })
+  }, [])
+
   async function handleLikeRepository(id) {
-    // Implement "Like Repository" functionality
+    const response = await api.post(`repositories/${id}/like`)  
+    const { likes } = response.data
+    const repositorieIndex = repositories.findIndex(repository => repository.id === id)
+    repositories[repositorieIndex].likes = likes
+    setRepositories([...repositories])  
+   
   }
 
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
       <SafeAreaView style={styles.container}>
-        <View style={styles.repositoryContainer}>
-          <Text style={styles.repository}>Repository 1</Text>
-
-          <View style={styles.techsContainer}>
-            <Text style={styles.tech}>
-              ReactJS
-            </Text>
-            <Text style={styles.tech}>
-              Node.js
-            </Text>
-          </View>
-
-          <View style={styles.likesContainer}>
-            <Text
-              style={styles.likeText}
-              // Remember to replace "1" below with repository ID: {`repository-likes-${repository.id}`}
-              testID={`repository-likes-1`}
-            >
-              3 curtidas
-            </Text>
-          </View>
-
-          <TouchableOpacity
-            style={styles.button}
-            onPress={() => handleLikeRepository(1)}
-            // Remember to replace "1" below with repository ID: {`like-button-${repository.id}`}
-            testID={`like-button-1`}
-          >
-            <Text style={styles.buttonText}>Curtir</Text>
-          </TouchableOpacity>
-        </View>
+        <FlatList
+          data={repositories}
+          keyExtractor={repository => repository.id}
+          renderItem={({ item : repository }) => {
+            return (
+              <View style={styles.repositoryContainer}>
+              <Text style={styles.repository}>{repository.title}</Text>
+              <View style={styles.techsContainer}>
+                {repository.techs.map(tech => <Text style={styles.tech}>{ tech }</Text>)}                                            
+              </View>
+                <View style={styles.likesContainer}>
+                  <Text
+                    style={styles.likeText}
+                    testID={`repository-likes-${repository.id}`}>
+                    {repository.likes} curtidas
+                  </Text>
+                  <TouchableOpacity
+                    activeOpacity={0.6}
+                    style={styles.button}
+                    onPress={() => handleLikeRepository(repository.id)}
+                    testID={`like-button-${repository.id}`}
+                  >
+                    <Text style={styles.buttonText}>Curtir</Text>
+                  </TouchableOpacity>
+                </View>
+              </View>
+            )            
+          }}>
+        </FlatList>
       </SafeAreaView>
     </>
   );
@@ -65,6 +78,7 @@ const styles = StyleSheet.create({
     marginHorizontal: 15,
     backgroundColor: "#fff",
     padding: 20,
+    borderRadius : 8,
   },
   repository: {
     fontSize: 32,
@@ -78,10 +92,10 @@ const styles = StyleSheet.create({
     fontSize: 12,
     fontWeight: "bold",
     marginRight: 10,
-    backgroundColor: "#04d361",
     paddingHorizontal: 10,
     paddingVertical: 5,
     color: "#fff",
+    backgroundColor: "#04d361",
   },
   likesContainer: {
     marginTop: 15,
@@ -95,13 +109,16 @@ const styles = StyleSheet.create({
   },
   button: {
     marginTop: 10,
+    borderRadius : 4,
+    borderWidth: 1,
+    backgroundColor: "#7159c1",
+    borderColor : "#7159c1",
   },
   buttonText: {
     fontSize: 14,
     fontWeight: "bold",
     marginRight: 10,
     color: "#fff",
-    backgroundColor: "#7159c1",
     padding: 15,
   },
 });
